24c75dae7600763f32872598b6bd50ac1669d223
sama004 <70210929+Sama-004@users.noreply.github.com>
2023-11-12 11:54:49 +0530

    added post request

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+var bodyParser = require("body-parser");
 var indexRouter = require("./routes/index");
 var newRouter = require("./routes/new");
 
@@ -13,6 +13,7 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,26 @@ const messages = [
     added: new Date(),
   },
 ];
+
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "New Message Form" });
+});
+
+// adding the new route
+router.post("/new", function (req, res, next) {
+  // Access the form data which is filled
+  const author = req.body.author;
+  const message = req.body.message;
+
+  // Adding new message to store it in the databse
+  const newmessage = {
+    text: message,
+    user: author,
+    added: new Date(),
+  };
+  messages.push(newmessage);
+  res.redirect("/");
+});
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Mini Messageboard", messages: messages });
 });
